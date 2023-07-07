47f2026c7f570e6723c4924dd526c06312b9b050
Vacarme27 <kevin.trillon@gmail.com>
2023-07-07 09:53:01 +0200

    timeout

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,8 +3,8 @@
 window.onload=function(){
     setTimeout(() => {
         document.querySelector(".loader").style.display="none"
-    }, 1000);        
-    // setTimeout(() => {        
+    }, 600);        
+    setTimeout(() => {        
         $(document).ready(function(){$(".gallery").mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:"myAwesomeLightbox",showTags:!0,tagsPosition:"top"})});				
-    // }, 500); 
+    }, 200); 
 }
