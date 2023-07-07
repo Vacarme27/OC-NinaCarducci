61f6b34647f044245bd45b0524b4f50d5e8668ac
Vacarme27 <kevin.trillon@gmail.com>
2023-07-07 09:25:41 +0200

    loader script

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,7 @@
 $(document).ready(function(){$(".gallery").mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:"myAwesomeLightbox",showTags:!0,tagsPosition:"top"})});
+
+window.onload=function(){
+    setTimeout(() => {				
+        document.querySelector(".loader").style.display="none"
+    }, 500);
+}	
